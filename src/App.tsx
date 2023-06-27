5559af4472d101a6eddbf20a51c8d802563e6c90
Diego <diegozar02@gmail.com>
2023-06-27 10:52:52 -0600

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,10 @@
+import { FC, ReactElement } from "react";
+import { Component } from "./interfaces/react_element";
+
+export const App: FC<Component> = ({}): ReactElement => {
+    return (
+        <div>
+            <h2>hola</h2>
+        </div>
+    );
+};
---
+++ src/interfaces/react_element.ts
@@ -0,0 +1,4 @@
+export interface Component {
+    className?: string;
+    children?: JSX.Element | JSX.Element[];
+}
