1abb79a401349d8437981f5864f87471e4422003
Diego <diegozar02@gmail.com>
2023-07-01 15:35:01 -0600

    created main structure

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,15 @@
-import { FC, ReactElement } from "react";
+import { FC, ReactElement, Fragment } from "react";
 import { Component } from "./interfaces/react_element";
 import { Outlet } from "react-router-dom";
+import { Header } from "./components/header/header";
 
 export const App: FC<Component> = ({}): ReactElement => {
     return (
-        <div>
-            <Outlet />
-        </div>
+        <Fragment>
+            <Header />
+            <main>
+                <Outlet />
+            </main>
+        </Fragment>
     );
 };
