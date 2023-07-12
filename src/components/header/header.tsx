e3fe05550a6aea3f9d385a131698b5a13ecbf696
Diego <diegozar02@gmail.com>
2023-07-12 00:26:51 -0600

    added create an account button

--- src/components/header/header.tsx
+++ src/components/header/header.tsx
@@ -2,24 +2,42 @@ import style from "../../scss/components/header.module.scss";
 import { Component } from "../../interfaces/react_element";
 import { BsCartFill } from "react-icons/bs";
 import { BiSolidUser } from "react-icons/bi";
-import { FC, ReactElement } from "react";
+import { FC, ReactElement, Fragment } from "react";
 import { Nav } from "./nav";
+import { useAuth } from "../../hooks/useAuth";
+import { Link } from "react-router-dom";
 
 export const Header: FC<Component> = ({}): ReactElement => {
+    const { AuthStatus } = useAuth();
+
+    let username: string = "";
+
+    if (AuthStatus.user)
+        username =
+            AuthStatus.user.name + " " + AuthStatus.user.lastname.slice(0, 1);
+
     return (
         <header className={style.header}>
             <h1 className={style.h1}>Snack Hunt</h1>
             <Nav />
             <div className={style.account}>
-                <p className={style.phone}>Diego Z</p>
-                <span className={style.info}>
-                    <button>
-                        <BsCartFill />
-                    </button>
-                    <button className={style.user}>
-                        <BiSolidUser />
-                    </button>
-                </span>
+                {username.length > 0 ? (
+                    <Fragment>
+                        <p className={style.phone}>{username}</p>
+                        <span className={style.info}>
+                            <button>
+                                <BsCartFill />
+                            </button>
+                            <button className={style.user}>
+                                <BiSolidUser />
+                            </button>
+                        </span>
+                    </Fragment>
+                ) : (
+                    <Link to="/auth/sign-up" className={style.noAuthBtn}>
+                        Create an account
+                    </Link>
+                )}
             </div>
         </header>
     );
