096a246ed6c1012c426ae0684698011cfc169988
Diego <diegozar02@gmail.com>
2023-07-13 21:36:59 -0600

    added page guard

--- src/pages/auth.page.tsx
+++ src/pages/auth.page.tsx
@@ -1,8 +1,15 @@
-import { FC, FormEvent, ReactElement, useState, Fragment } from "react";
+import {
+    FC,
+    FormEvent,
+    ReactElement,
+    useState,
+    Fragment,
+    useEffect,
+} from "react";
 import { Component } from "../interfaces/react_element";
 import styles from "../scss/pages/auth.module.scss";
 import GoogleIcon from "../assets/google_favicon.ico";
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 import { useAuth } from "../hooks/useAuth";
 import background from "../assets/auth_background.jpg";
 import { Select, SelectOption } from "../components/dropdown/dropdown";
@@ -10,6 +17,13 @@ import { ResSignUp } from "../interfaces/auth.interface";
 
 export const AuthPage: FC<Component> = ({}): ReactElement => {
     const mode = window.location.pathname.split("/auth/")[1];
+    const { SignIn, SignUp, GoogleSignUp, AuthStatus } = useAuth();
+
+    const navigate = useNavigate();
+
+    useEffect(() => {
+        AuthStatus.user && navigate("/");
+    }, []);
 
     const [email, setEmail] = useState("");
     const [password, setPassword] = useState("");
@@ -22,8 +36,6 @@ export const AuthPage: FC<Component> = ({}): ReactElement => {
         errors: [],
         message: "",
     });
-
-    const { SignIn, SignUp, GoogleSignUp } = useAuth();
 
     const options = [
         { label: "Male", value: "male" },
@@ -67,100 +79,108 @@ export const AuthPage: FC<Component> = ({}): ReactElement => {
 
     return (
         <div className={styles.authPage} onSubmit={Submit}>
-            <div className={styles.container}>
-                <form>
-                    <h2>{mode === "sign-up" ? "Sign up" : "Sign in"}</h2>
+            {!AuthStatus.user ? (
+                <div className={styles.container}>
+                    <form>
+                        <h2>{mode === "sign-up" ? "Sign up" : "Sign in"}</h2>
 
-                    <div className={styles.otherAuth}>
-                        <button
-                            type="button"
-                            className={styles.btn}
-                            onClick={
-                                mode === "sign-up" ? GoogleSignUp : () => {}
-                            }
-                        >
-                            <img src={GoogleIcon} alt="Google icon" />{" "}
-                            {mode === "sign-up" ? "Sign up" : "Sign in"} with
-                            Google
-                        </button>
-                    </div>
-
-                    <p className={styles.line}>or</p>
-
-                    <input
-                        type="email"
-                        placeholder="Email"
-                        value={email}
-                        onChange={(e) => setEmail(e.target.value)}
-                    />
-                    <input
-                        type="password"
-                        placeholder="Password"
-                        value={password}
-                        onChange={(e) => setPassword(e.target.value)}
-                    />
-
-                    {mode === "sign-up" && (
-                        <Fragment>
-                            <input
-                                type="text"
-                                placeholder="Name"
-                                value={name}
-                                onChange={(e) => setName(e.target.value)}
-                            />
-                            <input
-                                type="text"
-                                placeholder="Lastname"
-                                value={lastname}
-                                onChange={(e) => setLastname(e.target.value)}
-                            />
-
-                            <Select
-                                options={options}
-                                value={dropdown}
-                                onChange={(o) => {
-                                    setDropdown(o);
-                                    if (o && o.value) {
-                                        setGender(o.value as "female" | "male");
-                                    }
-                                }}
-                            />
-                        </Fragment>
-                    )}
-
-                    <button className={styles.btn} type="submit">
-                        {mode === "sign-up" ? "Sign up" : "Sign in"}
-                    </button>
-
-                    <p className={styles.instead}>
-                        {mode === "sign-up"
-                            ? "Already have an account?"
-                            : "Don't have an account?"}
-                        {mode === "sign-up" ? (
-                            <Link to="/auth/sign-in">Sign in instead.</Link>
-                        ) : (
-                            <Link to="/auth/sign-up">Sign up instead.</Link>
-                        )}
-                    </p>
-                    {response.message.length > 0 && (
-                        <div
-                            className={
-                                styles.box +
-                                (response.errors.length > 0
-                                    ? ` ${styles.red}`
-                                    : ` ${styles.green}`)
-                            }
-                        >
-                            <h4>{response.message}</h4>
+                        <div className={styles.otherAuth}>
+                            <button
+                                type="button"
+                                className={styles.btn}
+                                onClick={
+                                    mode === "sign-up" ? GoogleSignUp : () => {}
+                                }
+                            >
+                                <img src={GoogleIcon} alt="Google icon" />{" "}
+                                {mode === "sign-up" ? "Sign up" : "Sign in"}{" "}
+                                with Google
+                            </button>
                         </div>
-                    )}
-                </form>
-                <img
-                    className={styles.thumb}
-                    src={background}
-                    alt="cheese burger"
-                />
-            </div>
+
+                        <p className={styles.line}>or</p>
+
+                        <input
+                            type="email"
+                            placeholder="Email"
+                            value={email}
+                            onChange={(e) => setEmail(e.target.value)}
+                        />
+                        <input
+                            type="password"
+                            placeholder="Password"
+                            value={password}
+                            onChange={(e) => setPassword(e.target.value)}
+                        />
+
+                        {mode === "sign-up" && (
+                            <Fragment>
+                                <input
+                                    type="text"
+                                    placeholder="Name"
+                                    value={name}
+                                    onChange={(e) => setName(e.target.value)}
+                                />
+                                <input
+                                    type="text"
+                                    placeholder="Lastname"
+                                    value={lastname}
+                                    onChange={(e) =>
+                                        setLastname(e.target.value)
+                                    }
+                                />
+
+                                <Select
+                                    options={options}
+                                    value={dropdown}
+                                    onChange={(o) => {
+                                        setDropdown(o);
+                                        if (o && o.value) {
+                                            setGender(
+                                                o.value as "female" | "male"
+                                            );
+                                        }
+                                    }}
+                                />
+                            </Fragment>
+                        )}
+
+                        <button className={styles.btn} type="submit">
+                            {mode === "sign-up" ? "Sign up" : "Sign in"}
+                        </button>
+
+                        <p className={styles.instead}>
+                            {mode === "sign-up"
+                                ? "Already have an account?"
+                                : "Don't have an account?"}
+                            {mode === "sign-up" ? (
+                                <Link to="/auth/sign-in">Sign in instead.</Link>
+                            ) : (
+                                <Link to="/auth/sign-up">Sign up instead.</Link>
+                            )}
+                        </p>
+                        {response.message.length > 0 && (
+                            <div
+                                className={
+                                    styles.box +
+                                    (response.errors.length > 0
+                                        ? ` ${styles.red}`
+                                        : ` ${styles.green}`)
+                                }
+                            >
+                                <h4>{response.message}</h4>
+                            </div>
+                        )}
+                    </form>
+                    <img
+                        className={styles.thumb}
+                        src={background}
+                        alt="cheese burger"
+                    />
+                </div>
+            ) : (
+                <h2>no</h2>
+            )}
         </div>
     );
 };
