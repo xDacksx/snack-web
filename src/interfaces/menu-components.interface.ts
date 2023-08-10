7cadb1e0c2b5ea6e2743d940181f46764abee5f7
Diego <diegozar02@gmail.com>
2023-08-09 18:43:25 -0600

    created new menu interfaces

--- src/interfaces/menu-components.interface.ts
+++ src/interfaces/menu-components.interface.ts
@@ -3,8 +3,34 @@ export interface MenuItem {
     name: string;
     description: string;
     available: boolean;
+
+    imageId: number;
+    imageUrl: string;
+    quantity: number;
+    price: number;
+
     createdAt: Date;
     updatedAt: Date;
+}
+
+export interface MenuItemForm {
+    name: string;
+    description: string;
+    quantity: number;
+    available: boolean;
+    image: FileList;
+    price: number;
+}
+
+export interface MenuEditInfoForm {
+    name: string;
+    description: string;
+    quantity: number;
+    available: boolean;
+    price: number;
+}
+export interface MenuEditImgForm {
+    image: FileList;
 }
 
 export interface MenuFormAttributes {
