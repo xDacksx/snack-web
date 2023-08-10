34efe71cee74a29a0bd3636a3f32e22a3e055a42
Diego <diegozar02@gmail.com>
2023-08-10 15:11:15 -0600

    created map component

---
+++ src/components/contact/map.tsx
@@ -0,0 +1,24 @@
+import { FC, Fragment, ReactElement } from "react";
+import { Component } from "../../interfaces/react_element";
+import { GoogleMap, useJsApiLoader } from "@react-google-maps/api";
+7;
+
+export const Map: FC<Component> = ({}): ReactElement => {
+    const env = import.meta.env;
+
+    const { isLoaded } = useJsApiLoader({
+        id: "google-map-script",
+        googleMapsApiKey: env.VITE_MAPS_API_KEY,
+    });
+
+    const location: google.maps.LatLngLiteral = {
+        lat: 17.99733,
+        lng: -92.938835,
+    };
+
+    return (
+        <Fragment>
+            {isLoaded && <GoogleMap center={location} zoom={18} />}
+        </Fragment>
+    );
+};
