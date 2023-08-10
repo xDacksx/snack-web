2be01ee0421a19205d841c130efd204dd8fe33d7
Diego <diegozar02@gmail.com>
2023-08-10 15:11:54 -0600

    implemented contact component

--- src/pages/home.page.tsx
+++ src/pages/home.page.tsx
@@ -2,12 +2,14 @@ import { FC, ReactElement, Fragment } from "react";
 import { Component } from "../interfaces/react_element";
 import { Thumbline } from "../components/thumbline/thumbline";
 import { Menu } from "../components/menu/menu";
+import { ContactInfo } from "../components/contact/contact";
 
 export const HomePage: FC<Component> = ({}): ReactElement => {
     return (
         <Fragment>
             <Thumbline />
             <Menu />
+            <ContactInfo />
         </Fragment>
     );
 };
