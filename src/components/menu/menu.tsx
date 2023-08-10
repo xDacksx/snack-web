51574a1d311901426d8ef71a6d8b91877be836dc
Diego <diegozar02@gmail.com>
2023-08-09 20:06:41 -0600

    updated to menu only show available products

--- src/components/menu/menu.tsx
+++ src/components/menu/menu.tsx
@@ -7,18 +7,29 @@ import { useMenu } from "../../hooks/useMenu";
 export const Menu: FC<Component> = ({}): ReactElement => {
     const { MenuItems } = useMenu();
 
+    const isEmpty = MenuItems.filter((item) => item.available).length < 1;
+
     return (
         <div className={styles.menu}>
-            {MenuItems.map((product) => (
-                <MenuItem
-                    image={product.imageUrl}
-                    title={product.name}
-                    description={product.description}
-                    price={product.price}
-                    stars={4.5}
-                    key={product.id}
-                />
-            ))}
+            {MenuItems.map(
+                (product) =>
+                    product.available && (
+                        <MenuItem
+                            image={product.imageUrl}
+                            title={product.name}
+                            description={product.description}
+                            price={product.price}
+                            stars={4.5}
+                            key={product.id}
+                        />
+                    )
+            )}
+
+            {isEmpty && (
+                <p className={styles.empty}>
+                    Currently there are no products available :{"("}
+                </p>
+            )}
         </div>
     );
 };
