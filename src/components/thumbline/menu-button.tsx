9f3d44057741fdfd2f6c876e0fad563fd2d6e809
Diego <diegozar02@gmail.com>
2023-08-10 15:27:58 -0600

    fixed thumb button

--- src/components/thumbline/menu-button.tsx
+++ src/components/thumbline/menu-button.tsx
@@ -1,12 +1,11 @@
 import { FC, ReactElement } from "react";
 import { Component } from "../../interfaces/react_element";
-import { Link } from "react-router-dom";
 import { BiShoppingBag } from "react-icons/bi";
 
 export const MenuButton: FC<Component> = ({ className }): ReactElement => {
     return (
-        <Link className={className} to="/">
+        <a className={className} href="#menu">
             <BiShoppingBag /> See the menu
-        </Link>
+        </a>
     );
 };
