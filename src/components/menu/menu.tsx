f089382305b5ac02e2718d628a7b1e6796947315
Diego <diegozar02@gmail.com>
2023-08-09 19:28:41 -0600

    updated menu to fetch products

--- src/components/menu/menu.tsx
+++ src/components/menu/menu.tsx
@@ -2,35 +2,23 @@ import { FC, ReactElement } from "react";
 import { Component } from "../../interfaces/react_element";
 import styles from "../../scss/components/menu.module.scss";
 import { MenuItem } from "./menu-item";
-import hamImage from "../../assets/hamburguer.png";
+import { useMenu } from "../../hooks/useMenu";
 
 export const Menu: FC<Component> = ({}): ReactElement => {
-    const desc =
-        "The next range covers cases where the AR of the viewport is greater than 3/4 but less than 1/1. The next range covers casesssss";
+    const { MenuItems } = useMenu();
 
     return (
         <div className={styles.menu}>
-            <MenuItem
-                image={hamImage}
-                title="Double Beef"
-                description={desc}
-                price={20}
-                stars={4.5}
-            />
-            <MenuItem
-                image={hamImage}
-                title="Double Beef"
-                description={desc}
-                price={20}
-                stars={4.5}
-            />
-            <MenuItem
-                image={hamImage}
-                title="Double Beef"
-                description={desc}
-                price={20}
-                stars={4.5}
-            />
+            {MenuItems.map((product) => (
+                <MenuItem
+                    image={product.imageUrl}
+                    title={product.name}
+                    description={product.description}
+                    price={product.price}
+                    stars={4.5}
+                    key={product.id}
+                />
+            ))}
         </div>
     );
 };
