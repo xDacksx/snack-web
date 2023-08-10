2405a39f4d4345b6b3b96097d0b09e034b5d9158
Diego <diegozar02@gmail.com>
2023-08-09 18:42:49 -0600

    updated menu list to fetch from api

--- src/components/admin/menu-items/menu-list.tsx
+++ src/components/admin/menu-items/menu-list.tsx
@@ -1,52 +1,21 @@
-import { FC, Fragment, ReactElement, useEffect, useContext } from "react";
+import { FC, Fragment, ReactElement, useEffect } from "react";
 import { Component } from "../../../interfaces/react_element";
 import { PiHamburgerBold } from "react-icons/pi";
 import { GrFormAdd } from "react-icons/gr";
 import styles from "../../../scss/pages/admin.module.scss";
-import { MenuItem } from "../../../interfaces/menu-components.interface";
-import { MenuContext, MenuItemsType } from "../../../context/menu.context";
 import { Link } from "react-router-dom";
+import { useMenu } from "../../../hooks/useMenu";
 
 export const MenuList: FC<Component> = ({}): ReactElement => {
-    function newItem(id: number): MenuItem {
-        const item: MenuItem = {
-            id: id,
-            name: "Item",
-            description: "Lorem",
-            available: true,
-            createdAt: new Date(),
-            updatedAt: new Date(),
-        };
-        return item;
-    }
-
-    const { MenuItems, setMenuItems } = useContext(
-        MenuContext
-    ) as MenuItemsType;
-
-    function GetItems() {
-        const data = localStorage.getItem("menu-items");
-
-        if (!data) {
-            localStorage.setItem(
-                "menu-items",
-                JSON.stringify([newItem(1), newItem(2)])
-            );
-            setMenuItems([newItem(1), newItem(2)]);
-
-            return;
-        }
-        const items: MenuItem[] = JSON.parse(data);
-        setMenuItems(items);
-    }
+    const { MenuItems, updateProducts } = useMenu();
 
     useEffect(() => {
-        GetItems();
+        updateProducts();
     }, []);
 
     return (
         <Fragment>
-            <div className={styles.edit}>
+            <div className={styles.list}>
                 {MenuItems.map((item) => (
                     <Link
                         to={`item/${item.id.toString()}`}
