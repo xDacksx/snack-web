a1c16d904401a02265594b7fd738058e922934d4
Diego <diegozar02@gmail.com>
2023-08-12 15:53:39 -0600

    added download app button

--- src/components/menu/menu-item.tsx
+++ src/components/menu/menu-item.tsx
@@ -1,6 +1,7 @@
 import { FC, ReactElement } from "react";
 import styles from "../../scss/components/menu.module.scss";
 import { AiFillStar, AiOutlineStar } from "react-icons/ai";
+import { Link } from "react-router-dom";
 
 export const MenuItem: FC<MenuItem> = ({
     image,
@@ -35,9 +36,9 @@ export const MenuItem: FC<MenuItem> = ({
                     <p title={ogDesc}>{description}</p>
                 </div>
                 <span className={styles.quality}>
-                    <span className={styles.price}>
+                    <Link to="" className={styles.price}>
                         <span className={styles.ball} /> ${price.toFixed(2)}
-                    </span>
+                    </Link>
                     <span className={styles.stars}>
                         {stars_.map((_e, i) => (
                             <AiFillStar key={i} />
